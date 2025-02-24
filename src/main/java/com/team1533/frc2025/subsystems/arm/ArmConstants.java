a0cfdd7663e6e3005d6012fa92e64a163f055044
Kemchobro <vidhatu@gmail.com>
2025-02-23 21:48:54 -0500

    Update ArmConstants.java

--- src/main/java/com/team1533/frc2025/subsystems/arm/ArmConstants.java
+++ src/main/java/com/team1533/frc2025/subsystems/arm/ArmConstants.java
@@ -4,14 +4,14 @@ import com.team1533.frc2025.Constants.Gains;
 import com.team1533.frc2025.Constants;
 
 public class ArmConstants {
-    public static final int leaderTalonCanID = 233;
-    public static final int followerTalonCanID = 234;
+    public static final int leaderTalonCanID = 21;
+    public static final int followerTalonCanID = 22;
     public static final int pivotEncoderCanID = 340;
     public static final String canBUS = "rio";
 
-    public static final boolean leaderInverted = false;
+    public static final boolean leaderInverted = true;
 
-    public static final double reduction = 1;
+    public static final double reduction = 240.;
     public static final double SensorToMechanismRatio = 1.0;
     public static final double frictionVoltage = 0.25;
 
