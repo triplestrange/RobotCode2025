ec5ab021f5154700506724f5c426f3c0bab03116
Team 1533 Driver Station <frc1533@ecgrobotics.org>
2025-04-04 20:45:40 -0400

    Update at 'Fri Apr 04 20:45:40 EDT 2025'

--- src/main/java/com/team1533/frc2025/RobotContainer.java
+++ src/main/java/com/team1533/frc2025/RobotContainer.java
@@ -386,8 +386,8 @@ public class RobotContainer {
 
     // Operator Manual Wrist Override
     new Trigger(() -> Math.abs(operatorController.getRightY()) > 0.1)
-        .whileTrue(wristSubsystem.runDutyCycle(() -> 0.15 * operatorController.getRightY()))
-        .and(() -> operatorController.getRightY() > 0);
+        .and(() -> operatorController.getRightY() > 0)
+        .whileTrue(wristSubsystem.runDutyCycle(() -> 0.15 * operatorController.getRightY()));
 
     // // Operator Manual Elevator Override
     // new Trigger(
