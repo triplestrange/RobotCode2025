4d4ae2adbe08b830ac85d2c5fd7a4d16cbba9f5f
Team 1533 Driver Station <pablo.bear@yahoo.com>
2026-01-11 12:48:43 -0500

    missed a file

--- src/main/java/com/team1533/lib/subsystems/SimTalonFXWithCancoder.java
+++ src/main/java/com/team1533/lib/subsystems/SimTalonFXWithCancoder.java
@@ -1,42 +1,46 @@
-// package com.team1533.lib.subsystems;
+package com.team1533.lib.subsystems;
 
-// import edu.wpi.first.math.system.plant.DCMotor;
-// import edu.wpi.first.math.system.plant.LinearSystemId;
-// import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-// import java.util.function.Supplier;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import java.util.function.Supplier;
     
-// public class SimTalonFXWithCancoder extends SimTalonFXIO {
-//     MotorSubsystemWithCanCoderConfig canCoderConfig;    
+public class SimTalonFXWithCancoder extends SimTalonFXIO {
+    MotorSubsystemWithCanCoderConfig CanCoderConfig;
     
-//     public SimTalonFXWithCancoder(MotorSubsystemWithCanCoderConfig config) {
-//         super(
-//             config,
-//             new DCMotorSim(
-//             LinearSystemId.createDCMotorSystem(
-//                         DCMotor.getKrakenX60Foc(1),
-//                         config.momentOfInertia,
-//                         1.0 / config.ratioForSim),
-//                             DCMotor.getKrakenX60Foc(1),
-//                             0.001,
-//                             0.001));
-//             this.canCoderConfig = config;
-//         }
+    public class SimTalonFXWithCancoder extends SimTalonFXIO {
+        MotorSubsystemWithCanCoderConfig canCoderConfig;
     
-//         @Override
-//         public Supplier<SimCanCoderIO.SimCanCoderState> getSupplierForCancoder(
-//                 MotorSubsystemWithCanCoderConfig c) {
-//             return () -> {
-//                 var a = new SimCanCoderIO.SimCanCoderState();
-//                 double ratio =
-//                         this.canCoderConfig.ratioForSim / this.canCoderConfig.cancoderUnitsForSim;
-//                 a.positionRotations = lastRotations.get() * ratio;
-//                 a.velocityRotations = lastRPS.get() * ratio;
-//                 return a;
-//             };
-//         }
+        public SimTalonFXWithCancoder(MotorSubsystemWithCanCoderConfig config) {
+            super(
+                    config,
+                    new DCMotorSim(
+                            LinearSystemId.createDCMotorSystem(
+                                    DCMotor.getKrakenX60Foc(1),
+                                    config.momentOfInertia,
+                                    1.0 / config.ratioForSim),
+                            DCMotor.getKrakenX60Foc(1),
+                            0.001,
+                            0.001));
+            this.canCoderConfig = config;
+        }
     
-//         @Override
-//         protected double getSimRatio() {
-//             return canCoderConfig.ratioForSim;
-//         }
-//     }
+        @Override
+        public Supplier<SimCanCoderIO.SimCanCoderState> getSupplierForCancoder(
+                MotorSubsystemWithCanCoderConfig c) {
+            return () -> {
+                var a = new SimCanCoderIO.SimCanCoderState();
+                double ratio =
+                        this.canCoderConfig.ratioForSim / this.canCoderConfig.cancoderUnitsForSim;
+                a.positionRotations = lastRotations.get() * ratio;
+                a.velocityRotations = lastRPS.get() * ratio;
+                return a;
+            };
+        }
+    
+        @Override
+        protected double getSimRatio() {
+            return canCoderConfig.ratioForSim;
+        }
+    }
+}
